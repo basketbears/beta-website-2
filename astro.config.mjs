5ced9155198bcd86f1e57732527c6744d5b02348
heikomat <heiko.mathes@tuta.io>
2024-11-23 20:08:13 +0100

    add modal styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind(), purgecss()],
+  build: {
+    inlineStylesheets: 'never'
+  }
 });
